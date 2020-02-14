1a35438c98b2dbd803be70410842da7c5ecbf0f1
Aubrey Spannagel <aubreybohn@u.boisestate.edu>
2020-02-14 14:52:50 -0700

    created 'footer', created 'home', added h&f to home

---
+++ header.php
@@ -0,0 +1,22 @@
+<html>
+   <title> Aubrey's Thrift Finds </title>
+
+   <body>
+      
+      <img src="Logo.png" alt="Logo" title="Aubrey's Thrift Finds"/> 
+      
+      <ul>
+         <li><a href="home.php"> Home </a></li>
+         <li><a href="clothing.php"> Clothing </a></li>
+	 <li><a href="decor.php"> Decor </a></li>
+      </ul>
+      
+      <input type="text" placeholder="Search"/>
+      <input type="button" value="search"/>
+
+      <ul>
+         <li><a href="login.php"> Log In </a></li>
+         <li><a href="signup.php"> Sign Up </a></li>
+      </ul>
+      
+      <img src="shoppingCartIcon.jpg" alt="shoppingCartIcon" title="Shopping Cart"/>
---
+++ home.php
@@ -0,0 +1,5 @@
+<?php require_once 'header.php'; ?>
+
+   <div> hey its me, the home file </div>
+
+<?php require_once 'footer.php'; ?>
