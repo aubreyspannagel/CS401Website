9974f15813260a5fd85cc32aed29805742d5d756
Aubrey Spannagel <aubreybohn@u.boisestate.edu>
2020-02-14 16:57:50 -0700

    stylesheat.css created

--- header.php
+++ header.php
@@ -1,21 +1,24 @@
 <html>
    <title> Aubrey's Thrift Finds </title>
-
+   <link rel="stylesheet" type="text/css" href="stylesheat.css">
+   
    <body>
       
-      <img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/Logo.png" alt="Logo" title="Aubrey's Thrift Finds"/> 
-      <ul>
+      <div id="logo"> <img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/Logo.png" alt="Logo" title="Aubrey's Thrift Finds"/></div> 
+    
+      <ul id="subpages">
          <li><a href="home.php"> Home </a></li>
          <li><a href="clothing.php"> Clothing </a></li>
 	 <li><a href="decor.php"> Decor </a></li>
       </ul>
       
+
       <input type="text" placeholder="Search"/>
       <input type="button" value="search"/>
 
-      <ul>
+      <ul id="login">
          <li><a href="login.php"> Log In </a></li>
          <li><a href="signup.php"> Sign Up </a></li>
       </ul>
       
-      <img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/shoppingCartIcon.jpg" alt="shoppingCartIcon" title="Shopping Cart"/>
+      <div id="cartimage"><img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/shoppingCartIcon.jpg" alt="shoppingCartIcon" title="Shopping Cart"/></div>
