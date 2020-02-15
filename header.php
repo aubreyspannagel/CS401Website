2548c93b8b57ab02bc96fd8e45724f2be036ec03
Aubrey Spannagel <aubreybohn@u.boisestate.edu>
2020-02-14 20:47:47 -0700

    header tweeks

--- header.php
+++ header.php
@@ -3,27 +3,27 @@
       <title> Aubrey's Thrift Finds </title>
       <link rel="shortcut icon" href="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/favicon.png"/>
    </head>
-
    <link rel="stylesheet" type="text/css" href="stylesheat.css">
-   
    <body>
-      
-   <div id="search">  
-      <img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/logo1.png" alt="Logo" title="Aubrey's Thrift Finds"/> 
-    
-      <ul id="pagelinks">
+   <div id="logo">  
+      <img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/logo1.png" alt="Logo" title="Aubrey's Thrift Finds"/></div>     
+   <div>
+      <ul>
          <li class="subpages"><a href="home.php"> Home </a></li>
          <li class="subpages"><a href="clothing.php"> Clothing </a></li>
 	 <li class="subpages"><a href="decor.php"> Decor </a></li>
       </ul>     
-
+   </div> 
+   <div id="search">
       <input type="text" placeholder="Search"/>
       <input type="button" value="search"/>
-
+   </div>
+   <div>
       <ul id="login">
          <li class="log"><a href="login.php"> Log In </a></li>
          <li class="log"><a href="signup.php"> Sign Up </a></li>
       </ul>
-      
+   </div>
+   <div id="cart">
       <img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/cart.jpg" alt="shoppingCartIcon" title="Shopping Cart"/>
    </div>
