3b0cdf0065f0872d8ff281535626e23c49e883bc
Aubrey Spannagel <aubreybohn@u.boisestate.edu>
2020-02-17 15:10:02 -0700

    header tweeks

--- header.php
+++ header.php
@@ -7,23 +7,24 @@
    <body>
    <div id="logo">  
       <img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/logo1.png" alt="Logo" title="Aubrey's Thrift Finds"/></div>     
-   <div>
-      <ul>
+   <div id="mainDiv">
+     <div id="menu">
+       <ul>
          <li class="subpages"><a href="home.php"> Home </a></li>
          <li class="subpages"><a href="clothing.php"> Clothing </a></li>
 	 <li class="subpages"><a href="decor.php"> Decor </a></li>
-      </ul>     
-   </div> 
-   <div id="search">
-      <input type="text" placeholder="Search"/>
-      <input type="button" value="search"/>
-   </div>
-   <div id="cart">
-      <img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/cart.jpg" alt="shoppingCartIcon" title="Shopping Cart"/>
-   </div>
-   <div>
-      <ul>
+       </ul>     
+     </div> 
+     <div id="search">
+       <input type="text" placeholder="Search"/>
+     </div>
+     <div id="cart">
+       <img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/cart.jpg" alt="shoppingCartIcon" title="Shopping Cart"/>
+     </div>
+     <div id="login">
+       <ul>
          <li class="log"><a href="signup.php"> Sign Up </a></li>
          <li class="log"><a href="login.php"> Log In </a></li>
-      </ul>     
+       </ul>     
+     </div>
    </div> 
