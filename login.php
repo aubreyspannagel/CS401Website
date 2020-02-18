005c2ded2560256aa18105d3df4ad4a2ffd38187
Aubrey Spannagel <aubreybohn@u.boisestate.edu>
2020-02-17 19:31:24 -0700

    making login screen

--- login.php
+++ login.php
@@ -1,3 +1,10 @@
 <?php require_once 'header.php'; ?>
-<div id="login"> Login </div>
+<div id="login">
+  <div id="lgn"> Login 
+    <ul>
+      <li>Email <input type="text"></li>
+      <li>Password <input type="text"></li>
+    </ul>
+  </div> 
+</div>
 <?php require_once 'footer.php'; ?>
