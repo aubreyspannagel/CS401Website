56127fe6e7b00df38216d8d1feb481da2561380f
Aubrey Spannagel <aubreybohn@u.boisestate.edu>
2020-02-17 20:08:19 -0700

    making signup

--- signup.php
+++ signup.php
@@ -1,3 +1,13 @@
 <?php require_once 'header.php'; ?>
-<div id="signup"> Sign Up </div>
+<div id="signup">
+  <div id="snup"> Sign Up 
+    <ul>
+      <li class="emailpassword">First Name <input type="text"></li>
+      <li class="emailpassword">Last Name <input type="text"></li>
+      <li class="emailpassword">Email <input type="text"></li>
+      <li class="emailpassword">Password <input type="text"></li>
+      <li><button type="button">Create An Account</button></li>
+    </ul>
+  </div> 
+</div>
 <?php require_once 'footer.php'; ?>
