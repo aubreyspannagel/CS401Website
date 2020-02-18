3fa378c5d34821f6cd46c3433a49339638bdc412
Aubrey Spannagel <aubreybohn@u.boisestate.edu>
2020-02-17 17:02:25 -0700

    footer tweaks

--- footer.php
+++ footer.php
@@ -1,4 +1,21 @@
     <div id="footer">
+      <div id="about">
+        About
+        <div id="aboutLinks">
+          <a href="contactus.php"> Contact Us </a>
+          <a href="faq.php"> FAQ </a>
+        </div>
+      </div>
+      <div id="customerservice">
+        Customer Service
+        <div id="csLinks">
+          <a href="contactus.php"> Contact Us </a>
+          <a href="contactus.php"> Contact Us </a>
+        </div>
+      </div>
+      <div id="socialmedia">
+        Coming Soon!
+      </div>
     </div>  
   </body>
 </html>
