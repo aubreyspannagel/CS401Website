bb2fbf2f98d7ff408e4325dbf17c5042e24d4ad0
Aubrey Spannagel <aubreybohn@u.boisestate.edu>
2020-02-19 12:45:30 -0700

    clothing.php styling

--- clothing.php
+++ clothing.php
@@ -1,14 +1,13 @@
 <?php require_once 'header.php'; ?>
 <div id="clothing">
-  <div> 
+  <div id="clg"> 
     <ul>
       <li class="clothingmenu"> Living Room </li>
       <li class="clothingmenu"> Kitchen </li>
       <li class="clothingmenu"> Bedroom </li>
       <li class="clothingmenu"> Office </li>
     </ul>
-  </div>
-  <div id="clothingtitle"> <strong> Clothing </strong> </div>
+      <strong>Clothing</strong>
+  </div> 
 </div>
-
 <?php require_once 'footer.php'; ?>
