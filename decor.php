46f7af7f1f615b1257365521ddb8237cba944bc6
Aubrey Spannagel <aubreybohn@u.boisestate.edu>
2020-02-19 15:35:02 -0700

    clothing.php fixes

--- decor.php
+++ decor.php
@@ -1,3 +1,13 @@
 <?php require_once 'header.php'; ?>
-<div id="decor"> Decor </div>
+<div id="decor">
+  <div id="dcr">
+    <ul>
+      <li class="decormenu"> Living Room </li>
+      <li class="decormenu"> Kitchen </li>
+      <li class="decormenu"> Bedroom </li>
+      <li class="decormenu"> Office </li> 
+     </ul>
+  </div>
+  <div id="decortitle"> Decor </div>
+</div>
 <?php require_once 'footer.php'; ?>
