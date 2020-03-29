9f29b23b99ec66ea0b56c9aee7d7c7973d03eed1
Aubrey Spannagel <aubreybohn@u.boisestate.edu>
2020-03-28 18:20:32 -0600

    home and cart page

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
    <body>
    <div id="mainDiv">
      <div id="logo">  
-      <img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/logo1.png" alt="Logo" title="Aubrey's Thrift Finds"/>
+      <img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/logo1.png" id="logo" alt="Logo" title="Aubrey's Thrift Finds"/>
      </div>
      <div id="menu">
        <ul>
--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php require_once 'header.php'; ?>
-<div id="index"></div>
+<div id="home">
+ <a href="clothing.php"><img src="https://aubreysthriftfinds.s3-us-west-1.amazonaws.com/Home.png" id="homeimage" title="See what's new"/> </a>
+</div>
 
 <?php require_once 'footer.php'; ?>
