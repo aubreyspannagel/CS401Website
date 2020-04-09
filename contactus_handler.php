67f8c97ab61ee94aca1333207a515b1672f2a979
Aubrey Spannagel <aubreybohn@u.boisestate.edu>
2020-04-09 13:28:14 -0600

    contactus form validation

--- contactus_handler.php
+++ contactus_handler.php
@@ -5,12 +5,25 @@ $fn = $_POST['firstname'];
 $ln = $_POST['lastname'];
 $email = $_POST['email'];
 $comment = $_POST['comment'];
+$commentlengthmax = 500;
  
-  if(empty($fn) || empty($ln) || empty($email) || empty($comment)){
-    $_SESSION['contacterror'] = "Please fill all form fields.";
-  }else{
-    $_SESSION['contactmessage'] = "Your submission has been recieved! We will contact you shortly.";
-  }
+if(empty($fn) || empty($ln) || empty($email) || empty($comment)){
+  $_SESSION['contacterror'] = "Please fill all form fields.";
+}
+
+if(!ctype_alpha($fn) || !ctype_alpha($ln)){
+  $_SESSION['contacterror'] = "Only alpha characters allowed in name.";
+}
+
+if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+ $_SESSION['contacterror'] = "Invalid email.";
+}
+
+if(strlen($comment) > $commentlengthmax){
+ $_SESSION['contacterror'] = "Comment is too long.";
+}else{
+ $_SESSION['contactmessage'] = "Your submission has been recieved! We will contact you shortly.";
+}
 
 header("Location: localhost://contactus.php");
 exit;
